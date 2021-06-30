1ef80204eea04e688a271450cf9efa5a3ca644ef
thaidd-1069 <dang.dinh.thai@sun-asterisk.com>
2021-06-30 15:17:51 +0700

    Build Layout

---
+++ layouts/AppFooter.vue
@@ -0,0 +1,5 @@
+<template>
+    <footer class="text-center text-muted bg-light pt-2">
+    <p>{{ $t('project.name') }}</p>
+  </footer>
+</template>
---
+++ pages/app.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <app-header />
+        <router-view></router-view>
+        <app-footer />
+    </div>
+</template>
+<script>
+import AppHeader from '~/layouts/AppHeader.vue'
+import AppFooter from '~/layouts/AppFooter.vue'
+
+export default {
+  components: { AppHeader, AppFooter },
+}
+</script>
+
+<style>
+</style>
